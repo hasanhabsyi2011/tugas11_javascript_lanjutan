839d8a4e524273be7aeab69464dffdfcc9175cae
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-21 08:47:55 +0700

    Add files via upload

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+var dataBuah = ["Pisang", "Jeruk", "Apel", "Mangga"]
+
+console.log(dataBuah)
+
+var hasil= dataBuah.pop() //menghapus data terakhir
+console.log(dataBuah)
+
+
+dataBuah.shift()  //menghapus data pertama (index ke-0)
+console.log(dataBuah)
